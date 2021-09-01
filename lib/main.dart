505d591dfe3114bcb146a3e1241a43da3b788e2a
Dgryzhkov <dgryzhkov@gmail.com>
2021-09-01 13:03:07 +0700

    add state

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+@override
+_DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
+      return Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
               child: FlatButton(
-                onPressed: (){
-                  print('Left button got pressed');
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber=2;
+                    print('$leftDiceNumber');
+                  });
+                  },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('Right button got pressed');
                 },
                 child: Image.asset('images/dice1.png'),
               ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-                print('Right button got pressed');
-              },
-              child: Image.asset('images/dice1.png'),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
   }
-}
+
+
+
+
